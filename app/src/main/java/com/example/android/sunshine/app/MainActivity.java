62fda93b81e1892b3e949fd48c7238deab6a628a
Paulo Schneider <paulo.schneider@gmail.com>
2016-01-06 20:55:27 -0200

    Add fake forecast to view

--- app/src/main/java/com/example/android/sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/MainActivity.java
@@ -8,6 +8,11 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.Arrays;
+import java.util.List;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -57,6 +62,24 @@ public class MainActivity extends ActionBarActivity {
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+            List<String> fakeForecasts = Arrays.asList(
+                    "Today - Sunny - 88 / 63",
+                    "Tomorrow - Foggy - 70 / 46",
+                    "Weds - Cloudy - 72 / 63",
+                    "Thurs - Rainy - 64 / 51",
+                    "Fri - Foggy - 70 / 45",
+                    "Sat - Sunny - 76 / 68");
+
+            ArrayAdapter<String> forecastAdapter = new ArrayAdapter<>(
+                    getActivity(),
+                    R.layout.list_item_forecast,
+                    R.id.list_item_forecast_textview,
+                    fakeForecasts);
+
+            ListView listViewForecast = (ListView) rootView.findViewById(R.id.listview_forecast);
+            listViewForecast.setAdapter(forecastAdapter);
+
             return rootView;
         }
     }
