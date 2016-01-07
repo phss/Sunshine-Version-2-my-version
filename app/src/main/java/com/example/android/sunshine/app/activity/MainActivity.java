7a73524b0eb024fa4e16549639166761b653a48b
Paulo Schneider <paulo.schneider@gmail.com>
2016-01-07 17:40:04 -0200

    Create packages for components

--- app/src/main/java/com/example/android/sunshine/app/activity/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/activity/MainActivity.java
@@ -1,9 +1,12 @@
-package com.example.android.sunshine.app;
+package com.example.android.sunshine.app.activity;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.example.android.sunshine.app.R;
+import com.example.android.sunshine.app.fragment.ForecastFragment;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -21,23 +24,15 @@ public class MainActivity extends ActionBarActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (item.getItemId() == R.id.action_settings) {
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 
---
+++ app/src/main/java/com/example/android/sunshine/app/fragment/ForecastFragment.java
@@ -0,0 +1,74 @@
+package com.example.android.sunshine.app.fragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.example.android.sunshine.app.R;
+import com.example.android.sunshine.app.task.FetchWeatherTask;
+
+import java.util.ArrayList;
+
+public class ForecastFragment extends Fragment {
+
+    private ArrayAdapter<String> forecastAdapter;
+
+    public ForecastFragment() {
+
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.forecastfragment, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_refresh) {
+            new FetchWeatherTask(forecastAdapter).execute("94043");
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, final ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        forecastAdapter = new ArrayAdapter<>(
+                getActivity(),
+                R.layout.list_item_forecast,
+                R.id.list_item_forecast_textview,
+                new ArrayList());
+
+        ListView listViewForecast = (ListView) rootView.findViewById(R.id.listview_forecast);
+        listViewForecast.setAdapter(forecastAdapter);
+        listViewForecast.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String forecast = forecastAdapter.getItem(i);
+                Toast toast = Toast.makeText(container.getContext(), forecast, Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        });
+
+        return rootView;
+    }
+
+}
